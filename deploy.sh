008f24f9ef0adb5a836a24a2b6d1c3972aab7f67
qmliu <qmliu@pearlinpalm.com>
2019-04-03 14:50:26 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #install ack
 mkdir ~/bin
-curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
+#curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
+cp ./ack ~/bin/ack
+chmod 0755 ~/bin/ack
 #fot vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 cp .vimrc .gvimrc .pryrc ~/
