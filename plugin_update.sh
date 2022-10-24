15a3ad59a5baf48d21c27a0b9840fe171f8f8fd9
qmliu <qmliu@pearlinpalm.com>
2022-10-24 10:56:58 +0800

    add plugin_update sh

---
+++ plugin_update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# Author: javy liu
+#Date & Time: 2022-10-24 09:43:39
+#Description: git pull all vim plugin
+
+#change plugin src to git@github.com:xxx
+
+sed -i -E 's/https\:\/\/github.com\//git@github.com:/' ~/.vim/bundle/**/.git/config
+
+dir=$(ls -d ~/.vim/bundle/*)
+for folder in $dir ; do
+  echo "current is $folder"
+  cd $folder && git pull
+done
+
