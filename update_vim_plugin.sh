906c639ff7ac40f00f6c3ba3becd844e547cb5a3
qmliu <qmliu@pearlinpalm.com>
2023-03-15 12:34:55 +0800

    add update vim plugin shell

---
+++ update_vim_plugin.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Author: javy liu
+#Date & Time: 2023-03-15 11:59:09
+#Description: 用于更新所有插件
+
+vimpath="/home/`whoami`/.vim/bundle/"
+find $vimpath -name config | sed 's!git://github.com/!git@github.com:!'
+for fold in `ls $vimpath `; do
+  echo $fold
+  cd "$vimpath/$fold"
+  git pull
+  cd ..
+done
+
+
