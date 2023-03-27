dc174bae8b597c9937caf0cae8805501e69fbb38
qmliu <qmliu@pearlinpalm.com>
2023-03-27 16:09:14 +0800

    fix error on update_vim_plugin.sh

--- update_vim_plugin.sh
+++ update_vim_plugin.sh
@@ -4,7 +4,8 @@
 #Description: 用于更新所有插件
 
 vimpath="/home/`whoami`/.vim/bundle/"
-find $vimpath -name config | sed 's!git://github.com/!git@github.com:!'
+echo $vimpath
+find $vimpath -name config | xargs sed -i 's!git://github.com/!git@github.com:!'
 for fold in `ls $vimpath `; do
   echo $fold
   cd "$vimpath/$fold"
