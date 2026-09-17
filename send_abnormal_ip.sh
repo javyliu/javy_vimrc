dd24f5977b877e2145b46d13b85a762df002867f
qmliu <javy_liu@163.com>
2026-09-17 14:23:24 +0800

    add sshexe.sh

--- send_abnormal_ip.sh
+++ send_abnormal_ip.sh
@@ -8,6 +8,7 @@ logpath=/data3/access_logs/
 cd $logpath
 # $7 为user_agent,正常nginx日志应该是$4
 # gensub 替换长链接及以/xx?后面的内容
+true>uniq_ips.tmp
 cat web_access.log iphone_community.log | awk '{$4=gensub(/(\/[^\/]+)(\/[^\/]+|[?].*)+$/,"\\1","g", $4); a[$1"\t"$4]++}END{for(i in a) print i,a[i]}' | sort -k3nr | \
   awk '!seen[$1]++{order[++n]=$1}{a[$1]=a[$1]$0"\n"}END{for(i=1;i<=n;i++){
   print order[i] >> "uniq_ips.tmp"
@@ -18,7 +19,7 @@ ips=`head uniq_ips.tmp | xargs | sed 's/ /,/g'`
 #ips=`awk '{print $1}' ips.log | uniq| head | xargs | sed 's/ /,/g'`
 
 # 查询ip所在地区, 需先布署ipquery 容器，默认监听8080
-curl -s "http://localhost:3006/query?ip=$ips"|jq -r '.[]|.ip+"\t"+.country+"/"+.province+"/"+.city' > ip_with_area.tmp
+curl -s "http://localhost:3006/query?ip=$ips"|jq -r '.[]|.ip+"\t"+.country+"."+.province+"."+.city' > ip_with_area.tmp
 
 awk 'FILENAME==ARGV[1]{a[$1]=$2}FILENAME==ARGV[2]{
 b[$1]++
---
+++ sshexec.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: javy liu
+#Date & Time: 2026-09-17 12:47:58
+#Description: 主要用于在服务器执行简单命令，需提供服务器登入别名
+
+server_name=$1
+cmd=$2
+con=`gsed -nE "/\b$server_name\b/p" ~/.bash_profile|sed -E "s/.*'(.*)'.*/\1/"`
+
+if [ -z "$con" ]; then
+  echo "no alias"
+else
+  echo $con
+  $con bash <<-eof
+    $cmd
+eof
+fi
+
