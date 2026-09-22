4a4b73989995c397ee8bf7ef5bffd787d18799ff
qmliu <javy_liu@163.com>
2026-09-22 15:33:52 +0800

    update sed in linux

--- sshexec.sh
+++ sshexec.sh
@@ -5,7 +5,13 @@
 
 server_name=$1
 cmd=${@:2}
-con=`gsed -nE "/\b$server_name\b/p" ~/.bash_profile|sed -E "s/.*'(.*)'.*/\1/"`
+if command -v gsed &>/dev/null;then
+  SED="gsed"
+else
+  SED="sed"
+fi
+
+con=`$SED -nE "/\b$server_name\b/p" ~/.bash_profile|sed -E "s/.*'(.*)'.*/\1/"`
 
 if [ -z "$con" ]; then
   echo "no alias"
