eb43753974a367dd31bf9cb3a52d514c5ab86a21
qmliu <javy_liu@163.com>
2026-09-22 15:23:36 +0800

    update file

--- sshexec.sh
+++ sshexec.sh
@@ -4,15 +4,15 @@
 #Description: 主要用于在服务器执行简单命令，需提供服务器登入别名
 
 server_name=$1
-cmd=$2
+cmd=${@:2}
 con=`gsed -nE "/\b$server_name\b/p" ~/.bash_profile|sed -E "s/.*'(.*)'.*/\1/"`
 
 if [ -z "$con" ]; then
   echo "no alias"
 else
   echo $con
-  $con bash <<-eof
+  $con -o WarnWeakCrypto=no -T <<-EOF
     $cmd
-eof
+EOF
 fi
 
